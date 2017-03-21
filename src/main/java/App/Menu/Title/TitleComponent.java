07e6ffa5e530487efd2fa9f3dc99c52b5e4274c0
TheGuardianWolf <lichking21st@windowslive.com>
2017-03-22 06:53:29 +1300

    Added TitleComponent scaffolding.

--- src/main/java/App/Menu/Title/TitleComponent.java
+++ src/main/java/App/Menu/Title/TitleComponent.java
@@ -1,7 +1,18 @@
 package App.Menu.Title;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 /**
  * Created by lichk on 21/03/2017.
  */
 public class TitleComponent {
+    @FXML
+    Text play;
+
+    @FXML
+    Text settings;
+
+    @FXML
+    Text exit;
 }
