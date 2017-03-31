9319823d4e88277b64845b4e52bb32d03c159225
TheGuardianWolf <lichking21st@windowslive.com>
2017-03-31 20:23:08 +1300

    Partially completed physics service.
    Need to implement the ball bounce

--- src/main/java/App/Game/Ball/BallComponent.java
+++ src/main/java/App/Game/Ball/BallComponent.java
@@ -1,19 +1,22 @@
 package App.Game.Ball;
 
 import App.Game.GameService;
+import App.Game.Physics.Destructor;
 import App.Shared.SharedModule;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import warlordstest.IBall;
 
 import java.awt.*;
 
 /**
  * Created by lichk on 26/03/2017.
  */
-public class BallComponent {
+public class BallComponent implements IBall, Destructor {
     private SharedModule shared;
     private GameService game;
     private Image image;
+    private BallService model;
 
     public BallComponent(SharedModule shared, GameService game) {
         this.shared = shared;
@@ -21,6 +24,8 @@ public class BallComponent {
         this.image = this.shared.getJFX().loadImage(
                 this.getClass(), "BallComponent.png"
         );
+
+        this.model = game.getBall();
     }
 
     public void updateObject(Double intervalS) {
@@ -40,6 +45,66 @@ public class BallComponent {
                 position.y,
                 size.width, size.height
         );
+    }
+
+    /***
+     *  Set the horizontal position of the ball to the given value.
+     * @param x
+     */
+    public void setXPos(int x) {
+        this.model.getPosition().x = x;
+    }
+
+    /***
+     * Set the vertical position of the ball to the given value.
+     * @param y
+     */
+    public void setYPos(int y) {
+        this.model.getPosition().y = y;
+    }
+
+    /***
+     * @return the horizontal position of the ball.
+     */
+    public int getXPos() {
+        return ((int) this.model.getPosition().x);
+    }
+
+    /***
+     * @return the vertical position of the ball.
+     */
+    public int getYPos() {
+        return ((int) this.model.getPosition().y);
+    }
+
+    /***
+     *  Set the horizontal velocity of the ball to the given value.
+     * @param dX
+     */
+    public void setXVelocity(int dX) {
+        this.velocity.x = dX;
+    }
+
+    /***
+     *  Set the vertical velocity of the ball to the given value.
+     * @param dY
+     */
+    public void setYVelocity(int dY) {
+        this.velocity.y = dY;
+    };
+
+    /***
+     * @return the horizontal velocity of the ball.
+     */
+    public int getXVelocity() {
+        return ((int) this.velocity.getX());
+    }
+
+    /***
+     * @return the vertical velocity of the ball.
+     */
+    public int getYVelocity() {
+        return ((int) this.velocity.getY());
     }
 
 }
--- src/main/java/App/Game/Ball/BallService.java
+++ src/main/java/App/Game/Ball/BallService.java
@@ -7,7 +7,7 @@ import java.awt.*;
 /**
  * Created by lichk on 28/03/2017.
  */
-public class BallService implements IBall {
+public class BallService {
     private Point.Double position;
     private Point velocity;
     private Dimension size;
@@ -27,64 +27,4 @@ public class BallService implements IBall {
     }
 
     public Dimension getSize() { return this.size; }
-
-    /***
-     *  Set the horizontal position of the ball to the given value.
-     * @param x
-     */
-    public void setXPos(int x) {
-        this.position.x = x;
-    }
-
-    /***
-     * Set the vertical position of the ball to the given value.
-     * @param y
-     */
-    public void setYPos(int y) {
-        this.position.y = y;
-    }
-
-    /***
-     * @return the horizontal position of the ball.
-     */
-    public int getXPos() {
-        return ((int) this.position.getX());
-    }
-
-    /***
-     * @return the vertical position of the ball.
-     */
-    public int getYPos() {
-        return ((int) this.position.getY());
-    }
-
-    /***
-     *  Set the horizontal velocity of the ball to the given value.
-     * @param dX
-     */
-    public void setXVelocity(int dX) {
-        this.velocity.x = dX;
-    }
-
-    /***
-     *  Set the vertical velocity of the ball to the given value.
-     * @param dY
-     */
-    public void setYVelocity(int dY) {
-        this.velocity.y = dY;
-    };
-
-    /***
-     * @return the horizontal velocity of the ball.
-     */
-    public int getXVelocity() {
-        return ((int) this.velocity.getX());
-    }
-
-    /***
-     * @return the vertical velocity of the ball.
-     */
-    public int getYVelocity() {
-        return ((int) this.velocity.getY());
-    }
 }
